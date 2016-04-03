f92e5da764b26a3cfc69003a60d1bdb072baec27
Andreas <andreas.yuyi@gmail.com>
2016-04-02 19:10:54 -0700

    Add README. Finish the complete tag testing

--- infection_test.py
+++ infection_test.py
@@ -8,15 +8,41 @@ import sys
 class TestCompleteInfectionPlatform(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.platform = infection.Platform(file = "users_10000.json")
+        self.platform1 = infection.Platform(file = "data/users_100.json")
+        self.platform2 = infection.Platform(file = "data/users_10000.json")
 
     def testTotalInfection(self):
-        self.platform.generateGraph()
+        self.platform1.infection(15, '1.0.1')
+        self.platform1.infection(45, '1.2')
+        self.platform1.infection(75, '2.0')
+        self.assertEqual('1.0.1', self.platform1.getUserById(2).getVersion())
+        self.assertEqual('1.2', self.platform1.getUserById(39).getVersion())
+        self.assertEqual('2.0', self.platform1.getUserById(29).getVersion())
+        # self.platform1.generateGraph()
+
+        self.platform2.infection(4515, '1.0.1')
+        self.platform2.infection(146, '1.2')
+        self.platform2.infection(1463, '2.0')
+        self.assertEqual('1.0.1', self.platform2.getUserById(897).getVersion())
+        self.assertEqual('1.2', self.platform2.getUserById(176).getVersion())
+        self.assertEqual('2.0', self.platform2.getUserById(526).getVersion())
+
+    def testLimitedInfection(self):
+        self.assertEqual(False, self.platform1.limitedInfection(64, '1.0.1', 15))
+        self.assertEqual(True, self.platform1.limitedInfection(64, '1.0.1', 16))
+        self.assertEqual(False, self.platform1.limitedInfection(98, '2.0.1', 10))
+        self.assertEqual(True, self.platform1.limitedInfection(98, '2.0.1', 20))
+
+        self.assertEqual(False, self.platform2.limitedInfection(5502, '1.0.1'), 50)
+        self.assertEqual(True, self.platform2.limitedInfection(5502, '1.0.1'), 200)
+        self.assertEqual(False, self.platform2.limitedInfection(6288, '2.0.1'), 5)
+        self.assertEqual(True, self.platform2.limitedInfection(6288, '2.0.1'), 20)
+
 
 class TestSimpleInfectionPlatform(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.platform = infection.Platform(file = "users_15.json")
+        self.platform = infection.Platform(file = "data/users_15.json")
 
     def testTotalInfection(self):
         # Simple Total infection
